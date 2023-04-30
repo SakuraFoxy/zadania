256d649e57202b3957b2476a8df60638c4a2af27
SakuraFoxy <128425916+SakuraFoxy@users.noreply.github.com>
2023-04-30 13:27:29 +0200

    Add files via upload

---
+++ 6.8 i 6.10/Prostopadłościan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostopadłościan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostopadloscian i1 = new Prostopadloscian();
+            i1.dł=6; i1.sz=8; i1.wy=2; Console.WriteLine(i1.objetosc());
+            Prostopadloscian i2=new Prostopadloscian();
+            i2.dł=7; i2.sz=3; i2.wy=8; i1.Porobj(i2);
+        }
+    }
+    public class Prostopadloscian
+    {
+        public float dł {get;set;}
+        public float sz {get;set;}
+        public float wy {get;set;}
+        public float objetosc(){return dł*sz*wy;}
+        public void Porobj(Prostopadloscian i)
+        {
+            if (this.objetosc()>i.objetosc())Console.WriteLine("Pierwszy ma większ obj od drug");
+            else if(this.objetosc()<i.objetosc()) Console.WriteLine("Pierwszy ma mniej obj od drug");
+            else Console.WriteLine("Oba mają taką samą obj");
+        }
+    }
+}
---
+++ 6.8 i 6.10/Studia6.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studia6._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KandydatNaStud[] kan = new KandydatNaStud[3];
+            kan[0].nazw = "Kot";kan[0].pktMat = 57;kan[0].pktInf = 78;kan[0].pktJO = 66;
+            kan[1].nazw = "Ryba";kan[1].pktMat = 77;kan[1].pktInf = 46;kan[1].pktJO = 86;
+            kan[2].nazw = "Mak";kan[2].pktMat = 48;kan[2].pktInf = 89;kan[2].pktJO = 91;
+            foreach (KandydatNaStud kandydat in kan ){Console.WriteLine("{0}:{1} pkt", kandydat.nazw, kandydat.OblSumPkt());}
+            Console.ReadKey();
+        }
+    }
+    struct KandydatNaStud
+    {
+        public string nazw; public int pktMat, pktInf, pktJO;
+        public double OblSumPkt()
+        {double sumPkt=0.6*pktMat+0.5*pktInf+0.2*pktJO;return sumPkt;}
+    }
+
+}
